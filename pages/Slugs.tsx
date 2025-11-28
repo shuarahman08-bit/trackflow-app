3a60ecb070be2236858ead7ea0f0d38e7c45858a
shuarahman08-bit <shuarahman08@gmail.com>
2025-11-28 18:53:51 +0530

    Create Slugs.tsx

---
+++ pages/Slugs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Slugs: React.FC = () => {
+  return (
+    <div className="slugs">
+      <h2>Manage Slugs</h2>
+      <p>View and manage your tracking slugs here.</p>
+    </div>
+  );
+};
+
+export default Slugs;
