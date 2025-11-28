651312c5a283e5cb9424cdb26f11c62cbae6766d
shuarahman08-bit <shuarahman08@gmail.com>
2025-11-28 18:55:48 +0530

    Create Leads.tsx

---
+++ pages/Leads.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Leads: React.FC = () => {
+  return (
+    <div className="leads">
+      <h2>Leads</h2>
+      <p>View leads from your tracked videos.</p>
+    </div>
+  );
+};
+
+export default Leads;
